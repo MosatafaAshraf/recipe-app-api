06d0ae5d200e047874d9e594d2d86ab8b22fb01a
Ashraf, Mostafa <Mostafa.Ashraf@dell.com>
2023-07-22 16:55:29 +0300

    implement user API

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics
+from user.serializers import UserSerializers
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializers
