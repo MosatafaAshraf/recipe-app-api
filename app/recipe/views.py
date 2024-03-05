1f1630ae0752fa3a70c5a5ecee95540dc84af74e
Ashraf, Mostafa <Mostafa.Ashraf@dell.com>
2024-03-05 19:01:24 +0200

    fix linting error

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -29,7 +29,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.ListModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):      # noqa
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
